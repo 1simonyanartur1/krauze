3afdacfde9df29186a1d4939ecb0b0e6b733542f
Arthur Simonyan <1simonyanartur1@gmail.com>
2021-02-05 16:14:55 +0300

    fixes clippers page

---
+++ dist/js/common.js
@@ -0,0 +1,266 @@
+(function ($) {
+	$(document).ready(function () {
+
+		setTimeout(function () {
+			$('.crystal-wrap').addClass('active');
+			setTimeout(function () {
+				$('.crystal-wrap').removeClass('blink_on')
+			}, 3000);
+		}, 2000)
+
+		var $scrollPos = 200;
+		$(window).scroll(function () {
+			var $st = $(this).scrollTop();
+			if ($st > $scrollPos) {
+				// Скролл Вниз
+			} else {
+				// Скролл Вверх
+			}
+			if ($st > 700) {
+				$('.features-glue .bubbles').addClass('active');
+			}
+			$scrollPos = $st;
+		});
+
+		if ($(window).width() >= 1200) {
+			if ($("#prlx1").length) {
+				var scene1 = $('#prlx1').get(0);
+				var parallaxInstance1 = new Parallax(scene1);
+			} else {
+				console.log('#prlx1 not found on this page');
+			}
+			if ($("#prlx2").length) {
+				var scene2 = $('#prlx2').get(0);
+				var parallaxInstance2 = new Parallax(scene2);
+			} else {
+				console.log('#prlx2 not found on this page');
+			}
+			if ($("#prlx3").length) {
+				var scene3 = $('#prlx3').get(0);
+				var parallaxInstance3 = new Parallax(scene3);
+			} else {
+				console.log('#prlx3 not found on this page');
+			}
+		} else {
+			$('.universal-glue .images2').slick({
+				dots: false,
+				arrows: false,
+				infinite: true,
+				speed: 300,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				autoplay: true,
+				autoplaySpeed: 5000
+			});
+
+			$('.universal-glue .list li:first-child').addClass('active');
+
+			$('.universal-glue .images2').on('init, afterChange', function (event, slick, currentSlide, nextSlide) {
+				console.log(currentSlide);
+				$('.universal-glue .list li').each(function (e) {
+					if ($(this).data('slide') == currentSlide + 1) {
+						$('.universal-glue .list li').removeClass('active');
+						$(this).addClass('active');
+					}
+				});
+			});
+
+			$('.universal-glue .list li[data-slide]').on('click', function () {
+				var slideno = $(this).data('slide');
+				$('.universal-glue .images2').slick('slickGoTo', slideno - 1);
+			});
+		}
+
+		$('.reviews .slider').slick({
+			dots: true,
+			arrows: false,
+			speed: 300,
+			slidesToShow: 3,
+			slidesToScroll: 3,
+			responsive: [{
+					breakpoint: 1000,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2,
+					}
+				},
+				{
+					breakpoint: 700,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+					}
+				}
+			]
+		});
+
+		// $('.blades').slick({
+		// 	dots: false,
+		// 	arrows: true,
+		// 	prevArrow: '<button class="left icon-right-open-mini"></button>',
+		// 	nextArrow: '<button class="right icon-right-open-mini"></button>',
+		// 	infinite: true,
+		// 	speed: 300,
+		// 	slidesToShow: 1,
+		// 	slidesToScroll: 1,
+		// 	// autoplay: true,
+		// 	// autoplaySpeed: 5000,
+		// 	adaptiveHeight: true
+		// });
+
+		$('.new-clippers .slider').slick({
+			dots: false,
+			arrows: true,
+			prevArrow: '<button class="left icon-right-open-mini"></button>',
+			nextArrow: '<button class="right icon-right-open-mini"></button>',
+			infinite: true,
+			speed: 300,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			// autoplay: true,
+			// autoplaySpeed: 5000
+		});
+
+		// if ($(window).width() < 600) {
+		// 	$('.types-clippers .item__clipper').each(function () {
+		// 		var imgMobile = $(this).data('mobile');
+		// 		$(this).attr('src', imgMobile);
+		// 	});
+		// } else {}
+
+		$(".tabs").tabs();
+
+		// $('.types-clippers .item__clipper').on('mouseover', function () {
+		// 	$('.types-clippers .item__clipper').css({
+		// 		'opacity': '.5'
+		// 	})
+		// 	$(this).css({
+		// 		'opacity': '1'
+		// 	});
+		// });
+
+
+		var btn = $('.choose-folder .btns .btn1');
+		var formats = '';
+		var colors = '';
+		var quantity = '';
+
+		var imgFormat = 'a5';
+		var imgColor = 'black';
+		var imgSize = '96';
+		var f = 'А5+';
+		var c = 'classic';
+		var q = '48';
+		// Исходная ссылка
+		var startLink = 'https://www.erichkrause.com/catalog/Tetradi_i_bloknoty_s_plastikovoy_oblozhkoy_418/filter/model_name-is-folderbook/';
+
+		// Выделяем формат тетради
+		$(document).on('click', '.choose-folder .formats li', function () {
+			$('.choose-folder .formats li').removeClass('active');
+			$(this).addClass('active');
+
+			// У акцента и постеля нету а4, это проверка на это условие
+			if ($('.formats li:first-child').hasClass('active')) {
+				$('.colors.pastel, .colors.accent').addClass('disabled');
+
+				if($('.colors.pastel li, .colors.accent li').hasClass('active')) {
+					$('.colors.pastel li, .colors.accent li').removeClass('active');
+					$('.colors.classic li:first-child').addClass('active');
+					// $('.choose-folder__img').attr('src', '../img/choose-folder/classic-black-a4-48.png');
+					btn.attr('href', ``);
+					formats = 'uniq_props_format-is-a4/';
+					colors = 'item_collect_name-is-classic/uniq_props_covercolor-is-черный/';
+					changeUrl();
+
+					imgFormat = 'a4';
+					imgColor = 'black';
+					imgSize = '96';
+					f = 'А5+';
+					c = 'classic';
+					q = '2х48';
+					$('.choose-folder__img').attr('src', 'img/choose-folder/' + `${c}-${imgColor}-${imgFormat}-${imgSize}` + '.png');
+				} else {}
+
+			} else {
+				$('.colors.pastel, .colors.accent').removeClass('disabled');
+			}
+
+			formats = $(this).data('format');
+			changeUrl();
+		});
+		// Выделяем активный элемент цвета
+		$(document).on('click', '.choose-folder .colors li', function () {
+			$('.choose-folder .colors li').removeClass('active');
+			$(this).addClass('active');
+			colors = $(this).data('color');
+			changeUrl();
+		});
+		// Выделяем активный элемент кол-ва листов
+		$(document).on('click', '.choose-folder .quantity li', function () {
+			$('.choose-folder .quantity li').removeClass('active');
+			$(this).addClass('active');
+			quantity = $(this).data('quantity');
+			changeUrl();
+		});
+
+		function changeUrl() {
+			btn.attr('href', `${startLink}${formats}${colors}${quantity}apply/`);
+		}
+		function changeImg() {
+			// проверка отмечен ли формат
+			$('.choose-folder .formats li').on('click', function () {
+				f = $(this).text();
+				if(f == 'А4') {
+					imgFormat = 'a4'
+				} else if (f == 'А5+') {
+					imgFormat = 'a5'
+				}
+			});
+			// проверка отмечен ли цвет
+			$('.choose-folder .colors li').on('click', function () {
+				imgColor = $(this).attr('class');
+				if($(this).parents('.colors').hasClass('classic')) {
+					c = 'classic';
+				} else if ($(this).parents('.colors').hasClass('accent')) {
+					c = 'accent';
+				} else if ($(this).parents('.colors').hasClass('pastel')) {
+					c = 'pastel';
+				}
+			});
+			// проверка отмечен ли количество листов
+			$('.choose-folder .quantity li').on('click', function () {
+				q = $(this).text();
+				if(q == '48') {
+					imgSize = '48'
+				} else if (q == '2х48') {
+					imgSize = '96'
+				}
+			});
+			$('.choose-folder-block li').on('click', function () {
+				if(imgFormat.length != 0 && imgColor.length != 0 && imgSize.length != 0 && c.length != 0) {
+					$('.choose-folder__img').attr('src', 'img/choose-folder/' + `${c}-${imgColor}-${imgFormat}-${imgSize}` + '.png');
+				} else {}
+			});
+			
+		}
+
+		changeImg();
+
+		// Больше одного клика по активному элементу обработать
+
+	});
+
+	setTimeout(function () {
+		AOS.init({
+			offset: 50,
+			delay: 0,
+			duration: 1000,
+			once: true,
+			disable: ''
+		});
+	}, 500);
+
+})(jQuery);
+
+// $(window).on('load', function(){
+// });
---
+++ gulpfile.js
@@ -0,0 +1,238 @@
+'use strict';
+
+var gulp = require('gulp'),
+		pug = require('gulp-pug'),
+		sass = require('gulp-sass'),
+		autoprefixer = require('gulp-autoprefixer'),
+		formatHtml  = require('gulp-format-html'),
+		browserSync = require('browser-sync'),
+		concat = require('gulp-concat'),
+		cleanCSS = require('gulp-clean-css'),
+		sourcemaps = require('gulp-sourcemaps'),
+		clean = require('gulp-clean');
+
+var path = {
+	app: {
+		pug: './app/pug/**/*.pug',
+		html: './dist/',
+		sass: './app/sass/style.sass',
+		css: './dist/css/',
+		gulpfile: './gulpfile.js',
+		js: './dist/js/',
+		jsLibs: ['./app/libs/jquery/dist/jquery.min.js', './app/libs/slick-carousel/slick/slick.min.js', './app/libs/jquery-ui/jquery-ui.min.js'],
+		cssLibs: ['./app/libs/slick-carousel/slick/slick.css'],
+		deleteLibs: ['./app/js/libs.js', './app/css/libs.css']
+	}
+}
+
+// Compile pug to html
+gulp.task('pug', function() {
+	return gulp.src(path.app.pug)
+	.pipe(pug())
+	.pipe(formatHtml())
+	.pipe(gulp.dest(path.app.html))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+
+// Compile sass to css with autoprefixer
+gulp.task('sass', function() {
+	return gulp.src(path.app.sass)
+	.pipe(sourcemaps.init())
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	.pipe(autoprefixer ({
+		overrideBrowserslist: ['last 2 versions'],
+		cascade: false
+	}))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest(path.app.css))
+	.pipe(browserSync.reload({stream: true}))
+});
+
+// Task for browser-sync | auto-reload
+gulp.task('browser-sync', function() { 
+	browserSync({
+		server: {
+			baseDir: './dist/'
+		},
+		notify: false
+	});
+});
+
+// Auto reload browser on change js files
+gulp.task('scripts', function() {
+	return gulp.src(path.app.gulpfile)
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+// Delete libs files
+gulp.task('clean-libs', function () {
+	return gulp.src(path.app.deleteLibs)
+			.pipe(clean());
+});
+gulp.task('prod', function () {
+	return gulp.src(['./app/**'])
+			.pipe(gulp.dest('./dist/'));
+});
+gulp.task('clean-prod', function () {
+	return gulp.src(['./dist/libs/', './dist/sass/', './dist/pug/'])
+		.pipe(clean());
+});
+gulp.task('clean-dist', function () {
+	return gulp.src(['./dist/*'])
+		.pipe(clean());
+});
+
+// Concat js libraries
+gulp.task('concat-js', function() {
+	return gulp.src(path.app.jsLibs)
+	.pipe(concat('libs.js'), { allowEmpty: true })
+	.pipe(gulp.dest(path.app.js))
+	.pipe(browserSync.reload({stream: true}))
+});
+
+// Concat css libraries files
+gulp.task('concat-css', function() {
+	return gulp.src(path.app.cssLibs)
+	.pipe(concat('libs.css'), { allowEmpty: true })
+	.pipe(cleanCSS({compatibility: 'ie10'}))
+	.pipe(gulp.dest(path.app.css))
+	.pipe(browserSync.reload({stream: true}))
+});
+
+// Work without auto reload
+gulp.task('watch', function () {
+	gulp.watch(path.app.sass, gulp.series('sass'))
+	gulp.watch(path.app.pug, gulp.series('pug'))
+	gulp.watch([path.app.gulpfile, path.app.js], gulp.series('scripts'))
+});
+// Work with auto reload
+gulp.task('start', gulp.parallel('browser-sync', 'watch'));
+
+
+// 'use strict';
+
+// var gulp = require('gulp'),
+// 		pug = require('gulp-pug'),
+// 		sass = require('gulp-sass'),
+// 		autoprefixer = require('gulp-autoprefixer'),
+// 		htmlbeautify = require('gulp-html-beautify'),
+// 		browserSync = require('browser-sync'),
+// 		concat = require('gulp-concat'),
+// 		clean = require('gulp-clean');
+
+// var path = {
+// 	// Откуда брать
+// 	src: {
+// 		pug: './app/pug/pages/*.pug',
+// 		sass: ['./app/sass/style.sass'],
+// 		js: './app/js/*.js',
+// 		jsLibs: ['./app/libs/jquery/dist/jquery.min.js', './app/libs/slick-carousel/slick/slick.min.js', './app/libs/jquery-ui/jquery-ui.min.js'],
+// 		cssLibs: ['./app/libs/slick-carousel/slick/slick.css'],
+// 		deleteLibs: ['./dist/js/libs.js', './dist/css/libs.css']
+// 	},
+// 	// Куда компилировать
+// 	dist: {
+// 		html: './dist/',
+// 		css: './dist/css/',
+// 		js: './dist/js/',
+// 		jsLibs: './dist/js/',
+// 		cssLibs: './dist/css/'
+// 	}
+// }
+
+// var jsfiles = ['./dist/js/*.js', './dist/js/*.js', './gulpfile.js'];
+
+// // Compile pug to html with html-beautify
+// gulp.task('pug', function() {
+// 	var option = {
+//     "indent_size": 2,
+//     "indent_char": " ",
+//     "indent_level": 0,
+//     "indent_with_tabs": false,
+//     "preserve_newlines": true,
+//     "max_preserve_newlines": 10,
+//     "jslint_happy": false,
+//     "space_after_anon_function": false,
+//     "brace_style": "collapse",
+//     "keep_array_indentation": false,
+//     "keep_function_indentation": false,
+//     "space_before_conditional": true,
+//     "break_chained_methods": false,
+//     "eval_code": false,
+//     "unescape_strings": false,
+//     "wrap_line_length": 0,
+//     "wrap_attributes": "auto",
+//     "wrap_attributes_indent_size": 4,
+//     "end_with_newline": false
+// 	}
+// 	return gulp.src(path.src.pug)
+// 	.pipe(pug())
+// 	.pipe(htmlbeautify(option))
+// 	.pipe(gulp.dest(path.dist.html))
+// 	.pipe(browserSync.reload({stream: true}))
+// });
+
+// // Compile sass to css with autoprefixer
+// gulp.task('sass', function() {
+// 	return gulp.src(path.src.sass)
+// 	.pipe(sass().on('error', sass.logError))
+// 	.pipe(autoprefixer ({
+// 		overrideBrowserslist: ['last 2 versions'],
+// 		cascade: false
+// 	}))
+// 	.pipe(gulp.dest(path.dist.css))
+// 	.pipe(browserSync.reload({stream: true}))
+// });
+
+// // Task for browser-sync | auto-reload
+// gulp.task('browser-sync', function() { 
+// 	browserSync({
+// 		server: {
+// 			baseDir: './dist/'
+// 		},
+// 		notify: false
+// 	});
+// });
+
+// // Auto reload browser on change js files
+// gulp.task('scripts', function() {
+// 	return gulp.src(jsfiles)
+// 	.pipe(browserSync.reload({ stream: true }))
+// });
+
+// // Auto reload browser on change html
+// gulp.task('html', function() {
+// 	return gulp.src('./dist/*.html')
+// 	.pipe(browserSync.reload({ stream: true }))
+// });
+
+// // Delete libs files
+// gulp.task('delete-libs', function () {
+// 	return gulp.src(path.src.deleteLibs)
+// 			.pipe(clean());
+// });
+
+// // Concat js libraries
+// gulp.task('concat-js', function() {
+// 	return gulp.src(path.src.jsLibs)
+// 	.pipe(concat('libs.js'), { allowEmpty: true })
+// 	.pipe(gulp.dest(path.dist.jsLibs))
+// });
+
+// // Concat css libraries files
+// gulp.task('concat-css', function() {
+// 	return gulp.src(path.src.cssLibs)
+// 	.pipe(concat('libs.css'), { allowEmpty: true })
+// 	.pipe(gulp.dest(path.dist.cssLibs))
+// 	.pipe(browserSync.reload({stream: true}))
+// });
+
+// // Work without auto reload
+// gulp.task('watch', function () {
+// 	gulp.watch(path.src.sass, gulp.series('sass'))
+// 	gulp.watch(path.src.pug, gulp.series('pug'))
+// 	gulp.watch(jsfiles, gulp.series('scripts'))
+// });
+// // Work with auto reload
+// gulp.task('start', gulp.parallel('browser-sync', 'watch'));
